49c8e3139306813f702ec7bdf426462d7f252f8c
yiqiuzheng <yiqiuzheng@tencent.com>
2023-05-19 01:16:20 +0800

    fix: 文本内容长度汇总

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -517,11 +517,11 @@ export function Chat() {
           // TODO: 2、整个网页内容操作
           if(event.data.type === "read") {
             console.log('event.data', event.data.data.content);
-            // doSubmit(`请总结以下文本内容: ${event.data.data.content.join('')}`)
-            doSubmit('请总结以下文本内容，我将分为几段发送：')
-            event.data.data.content.forEach((item: string, index: number) => {
-              doSubmit(`第${index+1}段内容为: ${item}`)
-            })
+            doSubmit(`请总结以下文本内容: ${event.data.data.content.join('')}`)
+            // doSubmit('请总结以下文本内容，我将分为几段发送：')
+            // event.data.data.content.forEach((item: string, index: number) => {
+            //   doSubmit(`第${index+1}段内容为: ${item}`)
+            // })
           }
       }
     }, false);
