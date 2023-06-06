1c71bda71602d443e3d16e946ae9fe4278586cb1
yiqiuzheng <yiqiuzheng@tencent.com>
2023-06-06 11:09:17 +0800

    fix: 去掉无关

--- app/locales/cs.ts
+++ app/locales/cs.ts
@@ -73,19 +73,6 @@ const cs: LocaleType = {
     Lang: {
       Name: "Language", // ATTENTION: if you wanna add a new translation, please do not translate this value, leave it as `Language`
       All: "Všechny jazyky",
-      Options: {
-        cn: "简体中文",
-        en: "English",
-        tw: "繁體中文",
-        es: "Español",
-        it: "Italiano",
-        tr: "Türkçe",
-        jp: "日本語",
-        de: "Deutsch",
-        vi: "Vietnamese",
-        ru: "Русский",
-        cs: "Čeština",
-      },
     },
     Avatar: "Avatar",
     FontSize: {
